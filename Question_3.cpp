3802dddd69e35efa75ed7ae29baeafdc0b55fe37
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-22 20:23:49 +0530

    Create Question_3.cpp

---
+++ Question_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main() {
+int a,b,c,d,e,f,g;
+cout << "a = ";
+cin >> a;
+cout << "b = ";
+cin >> b;
+c = a + b;
+cout << "Sum = " << c << "\n";
+d = a - b;
+cout << "Subtraction = " << d << "\n";
+e = a * b;
+cout << "Multiplication = " << e << "\n";
+f = a / b;
+cout << "Quotient = " << f << "\n";
+g = a % b;
+cout << "Remainder = " << g;
+}
