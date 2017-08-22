6ad9f4ce8a6aa53809aa7cebf49d768e04782112
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-22 21:16:05 +0530

    Create Question_4.cpp

---
+++ Question_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main() {
+float a,b,c;
+cout << "Length = ";
+cin >> a;
+cout << "Breadth = ";
+cin >> b;
+c = 2 * (a + b);
+cout << "Perimeter = " << c;
+}
