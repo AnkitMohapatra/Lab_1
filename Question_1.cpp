460d5ce69512c889b0066535e780fe9742788d2d
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-22 20:13:48 +0530

    Create Question_1.cpp

---
+++ Question_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main() {
+int a;
+cout << "Enter a integer : ";
+cin >> a;
+cout << "Your integer is : " << a << "\n";
+float b;
+cout << "Enter a decimal : ";
+cin >> b;
+cout << "The decimal is : " << b;
+}
