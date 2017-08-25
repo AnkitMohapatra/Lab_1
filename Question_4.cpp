a8538cefa5074077c08f960dedf061ec485a4b20
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 11:55:50 +0530

    Update Question_4.cpp

--- Question_4.cpp
+++ Question_4.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-main() {
+int main() {
 float a,b;
 cout << "Length = ";
 cin >> a;
 cout << "Breadth = ";
 cin >> b;
 cout << "Perimeter = " << 2 * (a + b);
+return 0;
 }
