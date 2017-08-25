2f6f3e1f509c5a498510926532283224b3056fa5
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 11:57:44 +0530

    Update Question_6.cpp

--- Question_6.cpp
+++ Question_6.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-main() {
+int main() {
 float a;
 cout << "Radius : ";
 cin >> a;
 cout << "Diameter : " << 2 * a << "\n";
 cout << "Circumference : " << 2 * 3.14 * a << "\n";
 cout << "Area : " << 3.14 * a * a;
+return 0;
 }
